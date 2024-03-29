5967828933aa5e5d9b251df7d653d6314ee7a7a1
shungo0222 <kimura.shungo@gmail.com>
2024-03-29 16:48:42 +0900

    Create Escrow contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,18 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@thirdweb-dev/contracts/base/ERC20Base.sol";
+interface IERC20 {
+  function transfer(address to, uint256 amount) external returns (bool);
+  function balanceOf(address account) external view returns (uint256);
+}
 
-contract Escrow is ERC20Base {
-      constructor(
-        address _defaultAdmin,
-        string memory _name,
-        string memory _symbol
-    )
-        ERC20Base(
-            _defaultAdmin,
-            _name,
-            _symbol
-        )
-    {}
+contract Escrow {
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event Withdrawal(address indexed tokenAddress, address indexed to, uint256 amount);
+
+    constructor() {
+      owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+      require(msg.sender == owner, "You are not the owner");
+      _;
+    }
+
+    function withdraw(address tokenAddress, address to, uint256 amount) external onlyOwner {
+      IERC20 token = IERC20(tokenAddress);
+      require(token.balanceOf(address(this)) >= amount, "Insufficient token balance");
+      require(token.transfer(to, amount), "Transfer failed");
+      emit Withdrawal(tokenAddress, to, amount);
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+      require(newOwner != address(0), "Invalid address");
+      emit OwnershipTransferred(owner, newOwner);
+      owner = newOwner;
+    }
 }
